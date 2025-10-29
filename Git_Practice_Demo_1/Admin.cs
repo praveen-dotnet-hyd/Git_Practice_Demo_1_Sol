7f5c7fcc7d29088f9879ebf46012831f66fccf6c
Sadwale Shivaji <Sadwale.Shivaji@encora.com>
2025-10-29 18:27:31 +0530

    admin added

---
+++ Git_Practice_Demo_1/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Practice_Demo_1
+{
+    internal class Admin
+    {
+        public Admin() { }
+        public Admin(string name) { }
+        public Admin(string name, string description)
+        {
+        }
+    }
+}
